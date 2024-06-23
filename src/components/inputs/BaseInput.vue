b44cac34e2917549812be4e5a66678260124f1f3
Marcelo Oguihara <xamarc@outlook.com>
2024-06-23 12:41:15 -0300

    feat: Input field componentization

---
+++ src/components/inputs/BaseInput.vue
@@ -0,0 +1,44 @@
+<template>
+    <label v-if="label" :for=id class="field-label">{{ label }}</label>
+    <input  
+    :value="modelValue" 
+    @input="$emit('update:modelValue', $event.target.value)"
+    v-bind="$attrs"
+    :placeholder="placeholder"
+    :id="id"
+    class="field-input"
+    />
+</template>
+
+<script setup>
+  const props = defineProps({
+    label: {
+      type: [String, Boolean],
+      default: false,
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    id: {
+      type: String,
+      default: '',
+    }
+  });
+</script>
+<script>
+    export default {
+      data () {
+        return {
+          id: null
+        }
+      }, 
+      mounted(){
+        this.id = this.$.uid
+      }
+    }
+</script>
